0b8bed40350decb57856924256c4d80648515b03
liguangjie <zzj@163.com>
2022-08-09 14:56:40 +0800

    difference between IsNullOrEmpty with IsNullOrWhiteSpace

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,28 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("DemoFor SLFIRST!!!");
+            Console.WriteLine("--------Begin--------");
+            StringIsNull();
+            Console.WriteLine("--------End--------");
+        }
+        /// <summary>
+        /// String.IsNullOrEmpty and String.IsNullOrWhiteSpace
+        /// </summary>
+        public static void StringIsNull() 
+        {
+            string nullString = null;
+            string emptyString = "";
+            string whiteSpaceString = "   ";
+            string nonEmptyString = "abc123";
+            bool result;
+            Console.WriteLine(String.IsNullOrEmpty(nullString));                    //true
+            Console.WriteLine(String.IsNullOrEmpty(emptyString));                   //true
+            Console.WriteLine(String.IsNullOrEmpty(whiteSpaceString));              //false
+            Console.WriteLine(String.IsNullOrEmpty(nonEmptyString));                //false
+            Console.WriteLine(String.IsNullOrWhiteSpace(nullString));               //true
+            Console.WriteLine(String.IsNullOrWhiteSpace(emptyString));              //true
+            Console.WriteLine(String.IsNullOrWhiteSpace(whiteSpaceString));         //true
+            Console.WriteLine(String.IsNullOrWhiteSpace(nonEmptyString));           //false
         }
     }
 }
