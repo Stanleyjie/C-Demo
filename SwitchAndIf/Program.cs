b1a61257b42ba766b9520ec1354fe06231bd69b0
liguangjie <18814121574.com>
2023-04-16 23:52:29 +0800

    switch替代if

---
+++ SwitchAndIf/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SwitchAndIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            switch (num) 
+            {
+                case 1:
+                    Console.WriteLine(@$"{num}是1；");
+                    break;
+                case 10:
+                    Console.WriteLine($@"{num}是10");
+                    break;
+                default:
+                    Console.WriteLine(@$"啥也不是");
+                    break;
+            }
+        }
+    }
+}
